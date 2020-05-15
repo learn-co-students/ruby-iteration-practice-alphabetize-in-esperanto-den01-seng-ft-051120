0958e49c583174760bdaf260961a040b486ee0a4
Rene Avila <reneavila1993@gmail.com>
2020-05-15 04:23:44 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
+require 'pry'
 def alphabetize(arr)
-  # code here
+  alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+  arr.sort_by do |word|
+    word.split('').collect do |letter|
+    alphabet.index(letter)
+    end
+  end
 end
